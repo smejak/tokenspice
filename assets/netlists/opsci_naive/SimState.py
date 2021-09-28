760de59b6d9df3ea945bf94fa821bb7167ec989f
Jakub Smekal <jakub.smekal@gmail.com>
2021-09-28 16:31:25 +0100

    deleted replicate code

--- assets/netlists/opsci_naive/SimState.py
+++ assets/netlists/opsci_naive/SimState.py
@@ -6,7 +6,6 @@ from assets.agents.opsci_agents.ResearcherAgent import ResearcherAgent
 from assets.agents.opsci_agents.SellerAgent import SellerAgent
 from assets.agents.opsci_agents.OpsciMarketplaceAgent import OpsciMarketplaceAgent
 from assets.agents.opsci_agents.TokenBurnerAgent import TokenBurnerAgent
-from assets.agents.MinterAgents import OCEANLinearMinterAgent
 from assets.agents.RouterAgent import RouterAgent
 from engine import AgentBase, SimStateBase
 from .KPIs import KPIs
